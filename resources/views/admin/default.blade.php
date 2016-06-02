370e22882ed3487405f3bf97ca2f2a807d17da24
Kouceyla <hadjikouceyla@gmail.com>
2016-06-02 13:44:44 +0100

    Optimization of files

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -32,9 +32,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <header class="main-header">
 
       <!-- Logo -->
-      <a href="index2.html" class="logo">
+      <a href="/" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-mini"><b>L</b>LT</span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>Admin</b>LTE</span>
       </a>
