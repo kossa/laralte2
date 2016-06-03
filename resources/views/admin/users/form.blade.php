8d65444d9a11ab9cbf250e02fb302046161d0397
Kouceyla <hadjikouceyla@gmail.com>
2016-06-03 18:39:58 +0100

    Fix xss, fixes #3

--- resources/views/admin/users/form.blade.php
+++ resources/views/admin/users/form.blade.php
@@ -4,7 +4,7 @@
   <div class="col-sm-8">
     <div class="box box-info">
       <div class="box-header with-border">
-        <h3 class="box-title">{!! $title !!}</h3>
+        <h3 class="box-title">{{ $title }}</h3>
       </div>
       <div class="box-body">
 
