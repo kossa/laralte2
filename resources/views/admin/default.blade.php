311b7c9d529c47a09b2c0720b76d2f82c6159eee
kouceyla <hadjikouceyla@gmail.com>
2017-08-28 09:00:33 +0100

    change footer date

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -167,7 +167,7 @@
                 Anything you want
             </div>
             <!-- Default to the left -->
-            <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+            <strong>Copyright &copy; {{ date('Y') }} <a href="#">Company</a>.</strong> All rights reserved.
         </footer>
 
     </div>
